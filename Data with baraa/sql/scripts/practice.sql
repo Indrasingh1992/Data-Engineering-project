fe017fd20489d4bdd4e1f920418370f5e30c633b
IndraSingh1992 <Indrabhushan.singh@hotmail.com>
2026-02-26 18:16:00 +0530

    advance sql Subqueries sql

--- Data with baraa/sql/scripts/practice.sql
+++ Data with baraa/sql/scripts/practice.sql
@@ -35,10 +35,41 @@ WHERE DistRank <= 1;
 
 use SalesDB
 
-select Month(orderdate) OrderMonth, 
+select *, CurrentMonthSales - PreviousMonthSales as MOM_change,
+ROUND(cast((CurrentMonthSales - PreviousMonthSales) AS Float)/previousMonthSales*100,1)  as MOM_perc
+from
+(select Month(orderdate) OrderMonth, 
 sum(Sales) CurrentMonthSales,
 lag(sum(sales)) over (ORDER BY MONTH(OrderDate)) previousMonthSales
 from sales.Orders
-group by Month(orderdate)
+group by Month(orderdate))t
 
--- start with 12:13
+
+
+select
+CustomerID,
+AVG(DaysUnitnxtOrder) AvgDays,
+rank() over (ORDER BY COALESCE(AVG(DaysUnitnxtOrder), 999999)) Rankavg from
+(
+SELECT
+        OrderID,
+        CustomerID,
+        OrderDate,
+        LEAD(OrderDate) OVER(PARTITION BY CustomerID ORDER BY OrderDate) next_order,
+        DATEDIFF(day,OrderDate,LEAD(OrderDate) OVER(PARTITION BY CustomerID ORDER BY OrderDate)) DaysUnitnxtOrder
+from Sales.Orders)t
+GROUP BY CustomerID
+
+
+
+SELECT
+    OrderID,
+    ProductID,
+    Sales,
+    FIRST_VALUE(Sales) OVER (PARTITION BY ProductID ORDER BY Sales ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS LowestSales,
+    LAST_VALUE(Sales) OVER (PARTITION BY ProductID ORDER BY Sales ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS HighestSales,
+    Sales - FIRST_VALUE(Sales) OVER (PARTITION BY ProductID ORDER BY Sales ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) As SalesDifference
+    FROM Sales.Orders;
+
+
+    -- start  12:40:37 for advance sql Subqueries sql
