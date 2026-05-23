4c4f433de1d0bebd8f8b87670d89bebf7c06b313
Indrasingh1992 <indrabhushan.singh@hotmail.com>
2026-05-23 13:27:23 +0530

    completed Subquery

--- Data with baraa/sql/scripts/practice.sql
+++ Data with baraa/sql/scripts/practice.sql
@@ -160,3 +160,57 @@ SELECT
         CustomerID,
         SUM(Sales) AS TotalSales
     FROM Sales.Orders GROUP BY CustomerID) AS t
+
+
+/*
+====
+   SUBQUERY | COMPARISON OPERATORS
+===============================================================================*/
+use SalesDB
+/* TASK 6:
+   Find the products that have a price higher than the average price of all products.
+*/
+-- Main Query
+SELECT
+    ProductID,
+    Price,
+    (SELECT AVG(Price) FROM Sales.Products) AS AvgPrice -- Subquery
+FROM Sales.Products
+WHERE Price > (SELECT AVG(Price) FROM Sales.Products); -- Subquery
+
+
+
+SELECT
+    *
+FROM Sales.Orders
+WHERE CustomerID NOT IN (
+    -- Subquery
+    SELECT
+        CustomerID
+    FROM Sales.Customers
+    WHERE Country = 'Germany');
+    
+    use SalesDB
+/* TASK 9:
+   Find female employees whose salaries are greater than the salaries of any male employees.
+*/
+SELECT
+    EmployeeID, 
+    FirstName,
+    Salary
+FROM Sales.Employees
+WHERE Gender = 'F'
+  AND Salary > ALL (
+      SELECT Salary
+      FROM Sales.Employees
+      WHERE Gender = 'M'
+  );
+
+
+
+
+  use SalesDB
+/* TASK 10:
+   Show all customer details and the total orders for each customer using a correlated subquery.
+*/
+SELECT * , (Select count(*) from sales.Orders o where o.CustomerID = c.CustomerID) as TotalSales FROM Sales.customers c;
